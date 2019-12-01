6c6ba6b1862d3e3ea366bb42d8841b67a1dd67c7
Iván Homolicsan <homolicsan@gmail.com>
2019-12-01 19:53:20 -0300

    webpack y babel

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './public/javascripts/apps/results/index.js',
+    output: {
+      path: path.resolve(__dirname, 'public/javascripts/dist'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+           
+            
+          }
+        }
+      ]
+    }
+  }
