92379bd192b589d6cc0d6326b510d2fb62a661a4
Iván Homolicsan <homolicsan@gmail.com>
2019-12-02 09:49:50 -0300

    Results items

--- public/javascripts/apps/results/components/Item.js
+++ public/javascripts/apps/results/components/Item.js
@@ -2,7 +2,9 @@ import { h, render, Component } from 'preact';
 
 export const Item = (props) => (
 	<article>
+
 		<h2>{props.value.title}</h2>
+		<span> precio: {props.value.price.amount} </span>
 		<img src={props.value.picture} />
 	</article>
 )
