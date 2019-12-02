245e8bb2c18e8e3d6d8bbd3f5f9cd4f54db523fe
Iván Homolicsan <homolicsan@gmail.com>
2019-12-02 11:11:45 -0300

    max age, cache

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import indexRouter from './routes/index.js';
 import itemsRouter from './routes/items.js';
 import apiRouter from './routes/api.js';
 
+import expressStaticGzip  from 'express-static-gzip';
 
 
 import { dirname } from 'path';
@@ -18,6 +19,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -26,7 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(  path.join(__dirname, 'public'),  { maxage: 86400000 }   )  );
 
 app.use('/', indexRouter);
 app.use('/items', itemsRouter);
