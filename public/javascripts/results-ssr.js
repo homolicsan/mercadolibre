50b8c81890c7bf9c436a1f56ec604f0e53b48b24
Iván Homolicsan <homolicsan@gmail.com>
2019-12-02 11:10:57 -0300

    javascript inline

---
+++ public/javascripts/results-ssr.js
@@ -0,0 +1,12 @@
+
+ import { h, Component, render } from 'preact';
+
+
+
+  class App extends Component {
+    render() {
+      return <h1>Hello, world! aaaaaaa 454545</h1>;
+    }
+  }
+
+ export default App;
