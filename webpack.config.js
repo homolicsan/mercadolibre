953b9d9f50557c801c3598ac694500a38a0b43e9
Iván Homolicsan <homolicsan@gmail.com>
2019-12-02 02:33:32 -0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,22 +14,8 @@ module.exports = {
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader'
-           
-            
           }
-        },
-        {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: [
-              {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['react']
-                }
-              }
-            ],
-          }
+        }
       ]
     }
   }
