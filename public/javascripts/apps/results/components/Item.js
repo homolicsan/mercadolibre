715772f6a32f812022623550023dc653ff0c2c6d
Iván Homolicsan <homolicsan@gmail.com>
2019-12-03 02:19:22 -0300

    link a detail

--- public/javascripts/apps/results/components/Item.js
+++ public/javascripts/apps/results/components/Item.js
@@ -1,10 +1,11 @@
 import { h, render, Component } from 'preact';
 
-export const Item = (props) => (
-	<article>
+export const Item = (props) => {
+	let link ='/items/' + props.value.id;
 
-		<h2>{props.value.title}</h2>
+	return <article>
+		<h2><a href={link}>{props.value.title} {props.value.id} </a></h2>
 		<span> precio: {props.value.price.amount} </span>
 		<img src={props.value.picture} />
 	</article>
-)
+}
