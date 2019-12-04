08aa5c0a4086be10e708e547db74c7e5bcd79fa2
Iván Homolicsan <homolicsan@gmail.com>
2019-12-04 07:10:53 -0300

    Se modifica la respuesta de la api de ML para obtener una imagenes mas grande para el resultado de busqueda

--- helpers/items/responseDetailMapping.js
+++ helpers/items/responseDetailMapping.js
@@ -1,53 +1,52 @@
 // TODO: duplicado
 const formatPrice = (price) => {
-    let result = {};
+  let result = {};
 
-    price = price.toString();
-    price = price.split('.');
+  price = price.toString();
+  price = price.split('.');
 
-    result.amount = price.length ? parseInt(price[0]) : null;
-    result.decimals = price.length == 2? parseInt(price[1]) : 0;   
+  result.amount = price.length ? parseInt(price[0]) : null;
+  result.decimals = price.length == 2? parseInt(price[1]) : 0;   
 
-    return result;
+  return result;
 }
 
 const responseDetailMapping = (data_item, data_item_detail) => {
+  data_item = JSON.parse(data_item);
+  data_item_detail = JSON.parse(data_item_detail);
 
-    data_item = JSON.parse(data_item);
-    data_item_detail = JSON.parse(data_item_detail);
+  let new_response = {
+    author: {
+      name: "Iván",
+      lastname: "Homolicsan"
+    },
 
-    let new_response = {
-        author: {
-            name: "Iván",
-            lastname: "Homolicsan"
-        },
+    item : {}
+  };
 
-        item : {}
+  if (data_item){
+    let price =  formatPrice(data_item.price) ;
+
+    new_response.item = {
+      "id": data_item.id,
+      "title": data_item.title,
+      "price": {
+        "currency": data_item.currency_id,
+        "amount": price.amount,
+        "decimals": price.decimals
+      },
+      "picture": data_item.pictures[0].url,
+      "condition": data_item.condition,
+      "free_shipping": data_item.shipping.free_shipping,
+      "sold_quantity": data_item.sold_quantity
     };
+  }
 
-    if (data_item){
-        let price =  formatPrice(data_item.price) ;
+  if (data_item_detail && data_item_detail.plain_text) {
+      new_response.item.description = data_item_detail.plain_text;
+  }
 
-        new_response.item = {
-                "id": data_item.id,
-                "title": data_item.title,
-                "price": {
-                    "currency": data_item.currency_id,
-                    "amount": price.amount,
-                    "decimals": price.decimals
-                },
-                "picture": data_item.pictures[0].url,
-                "condition": data_item.condition,
-                "free_shipping": data_item.shipping.free_shipping,
-                "sold_quantity": data_item.sold_quantity
-            };
-    }
-
-    if (data_item_detail && data_item_detail.plain_text) {
-        new_response.item.description = data_item_detail.plain_text;
-    }
-
-    return new_response;
+  return new_response;
 };
 
 export default responseDetailMapping;
--- helpers/items/responseMapping.js
+++ helpers/items/responseMapping.js
@@ -2,50 +2,46 @@ const formatPrice = (price) => {
     let result = {};
 
     price = price.toString();
-
     price = price.split('.');
 
     result.amount = price.length ? parseInt(price[0]) : null;
-    result.decimals = price.length == 2? parseInt(price[1]) : 0;   
+    result.decimals = price.length == 2 ? parseInt(price[1]) : 0;   
 
     return result;
 }
 
-const makeResponse = (data) => {
-
+const make_response = (data) => {
     data = JSON.parse(data);
 
-    let newResponse = {
-        author: {
-            name: "Iván",
-            lastname: "Homolicsan"
-        },
-        categories: ["Categoria 1", "Sub categoria 2", "Subcategoria 3"] 
+    let new_response = {
+      author: {
+        name: "Iván",
+        lastname: "Homolicsan"
+      },
+      categories: ["Categoria 1", "Sub categoria 2", "Subcategoria 3"] 
     };
 
     if (data && data.results){
-        newResponse.items = data.results.map((data)=>{
+      new_response.items = data.results.map((data) => {
+        let price =  formatPrice(data.price) ;
 
-            let price =  formatPrice(data.price) ;
+        return {
+          "id": data.id,
+          "title": data.title,
+          "price": {
+              "currency": data.currency_id,
+              "amount": price.amount,
+              "decimals": price.decimals
+          },
+          "picture": data.thumbnail.replace('I.jpg','X.webp'),
+          "condition": data.condition,
+          "free_shipping": data.shipping.free_shipping 
+        };
 
-            return {
-                "id": data.id,
-                "title": data.title,
-                "price": {
-                    "currency": data.currency_id,
-                    "amount": price.amount,
-                    "decimals": price.decimals
-                },
-                "picture": data.thumbnail,
-                "condition": data.condition,
-                "free_shipping": data.shipping.free_shipping 
-            };
-
-        }); 
+      }); 
     }
 
-
-    return newResponse;
+  return new_response;
 };
 
-export default makeResponse;
+export default make_response;
